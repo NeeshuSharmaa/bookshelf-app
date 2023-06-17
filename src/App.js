54aa02b91084b744d0731e295cf12c5f785bccfc
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-06-17 19:31:52 +0530

    set up: routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Home from "./pages/home/Home";
+import Search from "./pages/search/Search";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
