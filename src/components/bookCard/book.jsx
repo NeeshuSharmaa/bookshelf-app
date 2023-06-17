9dcdd6873f89a43ecfe26cb32d3a87743746714c
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-06-17 19:24:59 +0530

    created- necessary files

---
+++ src/components/bookCard/book.jsx
@@ -0,0 +1,6 @@
+
+export default function book() {
+  return (
+    <div className="book-card">book</div>
+  )
+}
---
+++ src/pages/search/Search.js
@@ -0,0 +1,3 @@
+export default function Search() {
+  return <div className="search-page">Search</div>;
+}
