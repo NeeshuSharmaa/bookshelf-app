fe49795321b7a5710cd975de56863fe3a1820d80
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-06-17 19:56:49 +0530

    created: database of books

--- src/db.js
+++ src/db.js
@@ -1,1 +1,56 @@
-export const books = [];
+export const books = [
+  {
+    name: "One Hundred Names",
+    author: "Cecilia Ahern",
+    image: "https://pictures.abebooks.com/isbn/9780007350469-us.jpg",
+    category: "reading",
+  },
+  {
+    name: "And the Mountains Echoed",
+    author: "Khalid Hosseini",
+    image:
+      "https://m.media-amazon.com/images/I/41ylXaVUMXL._SX319_BO1,204,203,200_.jpg",
+    category: "reading",
+  },
+
+  {
+    name: "Where the Rainbows Ends",
+    author: "Cecilia Ahern",
+    image: "https://www.booksbykilo.in/media/books/BBK/9780007260829.jpg",
+    category: "read",
+  },
+  {
+    name: "The Nightangle",
+    author: "Kristin Hannah",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1681839850i/21853621.jpg",
+    category: "read",
+  },
+  {
+    name: "Untamed",
+    author: "Glennon Doyle",
+    image: "https://m.media-amazon.com/images/I/518J0uP0afS.jpg",
+    category: "read",
+  },
+  {
+    name: "Little Women",
+    author: "Louisa M. Alcott",
+    image:
+      "https://m.media-amazon.com/images/I/91l1EHFs8dL._AC_UF1000,1000_QL80_.jpg",
+    category: "tbr",
+  },
+  {
+    name: "Paper Towns",
+    author: "John Green",
+    image:
+      "https://m.media-amazon.com/images/I/81WBJQUEFsL._AC_UF1000,1000_QL80_.jpg",
+    category: "tbr",
+  },
+  {
+    name: "Memoirs of a Geisha",
+    author: "Arthur Golden",
+    image:
+      "https://m.media-amazon.com/images/I/71UgHbDujcL._AC_UF1000,1000_QL80_.jpg",
+    category: "tbr",
+  },
+];
