9cc3278e11b7f335f4bdbb4d47b613b442ea7a5c
Neeshu Sharma <114111237+NeeshuSharmaa@users.noreply.github.com>
2023-06-17 20:25:29 +0530

    built: nav

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Search from "./pages/search/Search";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <h3>The BookShelf</h3>
+        <Link to="/search">
+          <span>Search books</span>
+          <img src="search.svg" alt="search" />
+        </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
